30f43ae6630f6045e30894359a74a30df22ab323
cwalbanese <631gti@gmail.com>
2020-03-06 16:49:18 -0500

    updated css for navigation bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { Dropdown } from 'react-bootstrap';
 
 function Nav(props) {
   const logged_out_nav = (
-    <div>
-      <Link to="/login">login</Link>
-      <Link to="/signup">signup</Link>
-      <h3>{props.loggedIn ? `Hello, ${props.username}` : 'Please Log In'}</h3>
+    <div class="nav">
+      <div class="logo">
+        T<img class="gear" src="./images/gear-logo.png" alt="gear logo"></img>
+        TAL<span class="garage">GARAGE</span>
+      </div>
+      <div class="login">
+        <p>{props.loggedIn ? `Hello, ${props.username}` : 'Please Log In'}</p>
+        <Dropdown>
+          <Dropdown.Toggle variant="*" id="dropdown-basic">
+            <img class="user" src="./images/user.png" alt="user"></img>
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="/login">login</Dropdown.Item>
+            <Dropdown.Item href="/signup">signup</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
     </div>
   );
 
